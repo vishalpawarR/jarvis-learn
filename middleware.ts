28047c0a4cb34877953e960eefadf5de877cb931
Vishal Pawar <vishalpawar425@gmail.com>
2023-11-04 15:39:39 +0530

    Clerk gives a way for public routes

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,10 @@ import { authMiddleware } from "@clerk/nextjs";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({});
+export default authMiddleware({
+    // Uncomment below line to make for disabling the auth protection and for creating the protected line and create a route for the same in the app folder
+    // publicRoutes: ["/test"]
+});
 
 export const config = {
     matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
